f4e89664edb95c71668a59fec1b96df8fbbebbfe
liyingquan <liyingquan@67bf443c-83f4-11de-97a7-19ffed92f3b0>
2009-08-15 04:48:38 +0000

    like->ilike

--- src/org/powerstone/smartpagination/sample/UserModelQuery.java
+++ src/org/powerstone/smartpagination/sample/UserModelQuery.java
@@ -1,6 +1,7 @@
 package org.powerstone.smartpagination.sample;
 
 import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.powerstone.smartpagination.common.PageInfo;
@@ -23,16 +24,16 @@ public class UserModelQuery extends UserModel implements PageQuery<DetachedCrite
 		DetachedCriteria dc = DetachedCriteria.forClass(UserModel.class);
 		if (super.getUserName() != null && super.getUserName().trim().length() > 0) {
 			if (userNameLike) {
-				dc.add(Restrictions.like("userName", "%" + super.getUserName() + "%"));
+				dc.add(Restrictions.ilike("userName", "%" + super.getUserName() + "%"));
 			} else {
 				dc.add(Restrictions.eq("userName", super.getUserName()));
 			}
 		}
 		if (super.getEmail() != null && super.getEmail().trim().length() > 0) {
-			dc.add(Restrictions.like("email", "%" + super.getEmail() + "%"));
+			dc.add(Restrictions.ilike("email", "%" + super.getEmail() + "%"));
 		}
 		if (super.getRealName() != null && super.getRealName().trim().length() > 0) {
-			dc.add(Restrictions.like("realName", "%" + super.getRealName() + "%"));
+			dc.add(Restrictions.ilike("realName", "%" + super.getRealName() + "%"));
 		}
 		if (super.getSex() != null && super.getSex().trim().length() > 0) {
 			dc.add(Restrictions.eq("sex", super.getSex()));
