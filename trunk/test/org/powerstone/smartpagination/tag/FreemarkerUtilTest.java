9201b61bfc01f242f7d991e8609c65fccadec94c
liyingquan <liyingquan@67bf443c-83f4-11de-97a7-19ffed92f3b0>
2011-03-19 13:55:48 +0000

    完善ant脚本

--- trunk/test/org/powerstone/smartpagination/tag/FreemarkerUtilTest.java
+++ trunk/test/org/powerstone/smartpagination/tag/FreemarkerUtilTest.java
@@ -5,24 +5,26 @@ import java.util.HashMap;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 public class FreemarkerUtilTest extends TestCase {
+	Logger logger=Logger.getLogger(getClass());
 	@Test
 	public void testRentPageHead() {
 		HashMap<String, String> map = new HashMap<String, String>();
 		map.put("paging_fullUrl", "TTTT");
 		String html = FreemarkerUtil.rendPageHead(map);
-		System.out.println(html);
+		logger.debug(html);
 		Assert.assertTrue(html.indexOf("TTTT") > 0);
 	}
 
 	@Test
 	public void testRentPageBar() {
 		HashMap<String, String> map = new HashMap<String, String>();
-		map.put("paging_totalPages", "100");
+		map.put("paging_pageSizeParam", "ABCDE");
 		String html = FreemarkerUtil.rendPageBar(map);
-		System.out.println(html);
-		Assert.assertTrue(html.indexOf("100") > 0);
+		logger.debug(html);
+		Assert.assertTrue(html.indexOf("ABCDE.focus()") > 0);
 	}
 }
